63c1a8f3d2ec9d3b7c1732746f36a53aed9942d5
edusousa78 <sousa.ej@gmail.com>
2024-08-25 11:48:34 -0300

    definição de variáveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Luis";
+        double salarioMinimo = 2500;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println("----------------------------------------\n");
+        
+        System.out.print("Olá "+nome+"\nO Salario Mínomo é : "+salarioMinimo + 
+        " \nO Valor de PI é: "+ VALOR_DE_PI);
+
+        System.out.println("\n ----------------------------------------");
     }
 }
