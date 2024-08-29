6aa75e8aba8820f06000f825a43129d7d636a48d
edusousa78 <sousa.ej@gmail.com>
2024-08-29 10:03:46 -0300

    criada a classe operadores

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,39 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*int numero = 5;
+
+        System.out.println(numero);
+
+        System.out.println(++ numero);
+
+        numero = numero * numero -1;
+
+        System.out.println(numero);
+
+        numero++;
+        System.out.println(numero);
+        System.out.println(--numero);*/
+
+        /*int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);*/
+
+       /*  int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 != numero2;
+
+        System.out.println("Numero1 é Igual Numero2? " + simNao);*/
+        String nomeUm = "João";
+        String nomeDois = new String("Luis");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+    }
+
+}
